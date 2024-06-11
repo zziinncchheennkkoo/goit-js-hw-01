151a4b18db4248e791b7d217f0428b9fab733592
Irina Tropinina <zziinncchheennkkoo@gmail.com>
2024-06-11 21:33:10 +0300

    1

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,7 @@
 function getElementWidth(content, padding, border) {
-    return Namber.parseFloat(content) + (Namber.parseFloat(padding) * 2) + (Namber.parseFloat(border) * 2);
+    return Number.parseFloat(content) + (Number.parseFloat(padding) * 2) + (Number.parseFloat(border) * 2);
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px")); 
-
-
-// console.log(getElementWidth("50px", "8px", "4px")); // 74
-// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-// console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth("50px", "8px", "4px"));     // 74
+console.log(getElementWidth("60px", "12px", "8.5px"));  // 101
+console.log(getElementWidth("200px", "0px", "0px"));    // 200
