d1f00d9b0b22840112a9a4679be1783bd542ca04
Irina Tropinina <zziinncchheennkkoo@gmail.com>
2024-06-11 19:58:52 +0300

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,23 +1,11 @@
-const quantity = 5;
-const pricePerDroid = 3000;
-const totalPrice = 15000;
-const makeTransaction = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-console.log(makeTransaction);
-
-const quantity = 3;
-const pricePerDroid = 1000;
-const totalPrice = 3000;
-const makeTransaction = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-console.log(makeTransaction);
-
-const quantity = 10;
-const pricePerDroid = 500;
-const totalPrice = 5000;
-const makeTransaction = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-console.log(makeTransaction);
+function makeTransaction(quantity, pricePerDroid) {
+    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+}
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
 
 
-
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
